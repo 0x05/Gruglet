4407903bd76594bb47af1f3207a146710fca6433
Anon <gitmostly@gmail.com>
2018-07-15 20:38:42 +0300

    Removed debug code

--- api_idex.py
+++ api_idex.py
@@ -2,16 +2,13 @@ import requests
 
 API_URL = 'https://api.idex.market/returnTicker/'
 
-def returnTicker(ticker):
-      
-    #print(f'{ticker}') #dbg
-  
+
+def returnTicker(ticker):      
     params = dict(
         market='ETH_' + ticker
     )
     response = requests.post(API_URL, json=params)
     if response.ok:
-        
         rdata = response.json()
 
         last = rdata['last']
@@ -21,8 +18,6 @@ def returnTicker(ticker):
         vol = rdata['baseVolume']
 
         fdata = f'`Last: {last:.10}\tHigh: {high:.10}\tLow: {low:.10}\tChange: {pc:.4}%\t Volume: {vol:.6}ETH`'
-
-        #print(f'{rdata}')
     else:
         if response.status_code == 504:
             return 'Idex is slow, try again'
